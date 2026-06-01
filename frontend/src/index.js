b1fd8a006801c5831220800c19d7828402f04498
Wesley <wutzel@derlenny.de>
2026-06-01 21:51:33 +0200

    Navigationsleiste reiner style ohne Routen

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+import App from './App';
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <App />
+);
