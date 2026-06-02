da64f54f88f6a98213206665cecff37d2003c564
´Lucas Mauermann <lmauermann@stud.hs-bremen.de>
2026-06-02 22:52:57 +0200

    added footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Footer from "./Footer";
+
+
+function App(){
+    return(
+        <>
+        <h1>TEST</h1>
+        <Footer></Footer>
+        </>
+    );
+}
+
+export default App
