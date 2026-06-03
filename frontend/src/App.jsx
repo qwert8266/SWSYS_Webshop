f77a6dacbd10fff745c2fb82df76393730f778f5
´Lucas Mauermann <lmauermann@stud.hs-bremen.de>
2026-06-03 18:14:26 +0200

    merged with register and implemented login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from 'react-router-dom';
 
 import Register from "./pages/register";
+import Login from "./pages/login"
 import Navbar from './components/navbar';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />}/>
       </Routes>
       <Footer></Footer>
     </>
