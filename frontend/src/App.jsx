1f24cbfb89fe5ab9fdaf6beb21297a983fa5319a
Wesley <wutzel@derlenny.de>
2026-06-08 19:45:08 +0200

    implemented protected route for account-settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
+import ProtectedRoutes from "./routes/protectedRoutes";
+
 import Register from "./pages/register";
 import Login from "./pages/login"
 import Navbar from './components/navbar';
@@ -28,7 +30,12 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />}/>
           <Route path="/cart" element={<ShoppingCart />} />
-          <Route path="account_settings" element={<AccountSettings/>}/>
+          <Route path="account-settings" element={
+            <ProtectedRoutes>
+              <AccountSettings/>
+            </ProtectedRoutes>
+            }
+          />
           <Route path="/bier" element={<Category key="bier" products={biere} banner={top_banners[0]} category={"bier"}/>} />
           <Route path="/wein" element={<Category key="wein" products={weine} banner={top_banners[1]} category={"wein"}/>} />
           <Route path="/schnaps" element={<Category key="schnaps" products={schnäpse} banner={top_banners[2]} category={"schnaps"}/>} />
