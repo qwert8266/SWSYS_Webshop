dd49b83ea096d6d5cc61825157b250a9d37deb33
Wesley <wutzel@derlenny.de>
2026-07-10 14:54:17 +0200

    ugly confirmation Message when adding a product to the shopping cart and visivle in the navbar which category page your are on

--- frontend/src/pages/product.jsx
+++ frontend/src/pages/product.jsx
@@ -147,7 +147,7 @@ function Product(){
     return(
         <div className='product-page'>
             {loadError && <p className='text-danger'>{loadError}</p>}
-            {cartMessage && <p className='text-success'>{cartMessage}</p>}
+            {/*{cartMessage && <p className='text-success'>{cartMessage}</p>} */}
             
             <div className='product-page-top'>
                 <div className='d-flex flex-column'>
