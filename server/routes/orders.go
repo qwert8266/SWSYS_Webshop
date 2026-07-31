c8bfdabb8a83e8430e93508d479d095ef468e998
Nime05 <nico.mestrup@gmail.com>
2026-07-31 18:30:35 +0200

    Add 'owner' role to protected order routes

--- server/routes/orders.go
+++ server/routes/orders.go
@@ -15,7 +15,7 @@ func RegisterOrderRoutes(orderRoutes *gin.RouterGroup) {
 	orderRoutes.POST("/:id/return-request", handlers.RequestOrderReturn)
 
 	protectedOrderRoutes := orderRoutes.Group("")
-	protectedOrderRoutes.Use(middleware.RoleAuth("admin", "worker"))
+	protectedOrderRoutes.Use(middleware.RoleAuth("admin", "worker", "owner"))
 	{
 		protectedOrderRoutes.GET("/statistics", handlers.GetStatistics)
 		orderRoutes.GET("/", handlers.GetOrders)
